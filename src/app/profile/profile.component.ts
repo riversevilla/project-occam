31759504ea03e09f4c14e926fa8d9288939d3660
Rio Sevilla <rjs.sevilla@gmail.com>
2022-10-11 06:03:38 +0800

    delete unused files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,8 +3,7 @@ import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
 
