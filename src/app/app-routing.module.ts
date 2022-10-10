059fc1c794f1823c7c4d04425851d336f13cb0b7
Rio Sevilla <rjs.sevilla@gmail.com>
2022-10-10 18:21:09 +0800

    add assets;create header, footer, welcome, profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent},
+  { path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
